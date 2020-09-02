cccad67cb85ef55ca4605c7244d2b67bca968122
irfoooo <70160451+irfoooo@users.noreply.github.com>
2020-09-02 10:44:11 +0530

    Add files via upload

---
+++ MULTIPLE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int a,b,c,sum;
+void main()
+{
+clrscr();
+printf("\n enter three numbers:");
+scanf("%d%d%d",&a,&b,&c);
+sum=a+b+c;
+printf("\n the sum of three number is=%d",sum);
+getch();
+}
